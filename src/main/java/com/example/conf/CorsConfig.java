1fa1071026133f2d33327f865d20f4085757dd1a
denis <den_m1985@mail.ru>
2025-07-14 18:31:41 +0200

    added profile for test

--- src/main/java/com/example/conf/CorsConfig.java
+++ src/main/java/com/example/conf/CorsConfig.java
@@ -1,12 +1,14 @@
 package com.example.conf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Profile("!test")
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
